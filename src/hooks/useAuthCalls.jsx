4e4cc9a05925cf8c0b7b03fec47fb883499f9f0d
AhmetBoyraz01~ <ahmetboyraz.de@gmail.com>
2022-11-22 16:15:36 +0100

    Icons

--- src/hooks/useAuthCalls.jsx
+++ src/hooks/useAuthCalls.jsx
@@ -15,7 +15,7 @@ const useAuthCalls = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const BASE_URL = "https://10001.fullstack.clarusway.com/";
+  const BASE_URL = "https://14208.fullstack.clarusway.com/";
 
   const login = async (userInfo) => {
     dispatch(fetchStart());
