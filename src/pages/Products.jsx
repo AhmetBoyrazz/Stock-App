ee4add50bc85f79cd5819cc7278b10dc99e7ae02
AhmetBoyraz01~ <ahmetboyraz.de@gmail.com>
2022-11-25 14:47:58 +0100

    Products.jsx

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,5 +1,70 @@
+import { useEffect, useState } from "react";
+import useStockCalls from "../hooks/useStockCalls";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import DeleteIcon from "@mui/icons-material/Delete";
+import UpgradeIcon from "@mui/icons-material/Upgrade";
+import VerticalAlignBottomIcon from "@mui/icons-material/VerticalAlignBottom";
+
+import { useSelector } from "react-redux";
+import { arrowStyle, btnHoverStyle } from "../styles/globalStyle";
+
 const Products = () => {
-  return <div>Products</div>;
+  const { getBrands, getCategories, getProducts } = useStockCalls();
+  const { products } = useSelector((state) => state.stock);
+  const [open, setOpen] = useState(false);
+  const [info, setInfo] = useState({});
+
+  useEffect(() => {
+    getBrands();
+    getCategories();
+    getProducts();
+  }, []);
+
+  //? Siralanacak local state (sutun verilerinin local state hali)
+  const [sortedProducts, setSortedProducts] = useState(products);
+
+  //! product state'i her guncellendiginde local state'i de guncelle
+  useEffect(() => {
+    setSortedProducts(products);
+  }, [products]);
+
+  const [toggle, setToggle] = useState({
+    brand: 1,
+    name: 1,
+    stock: 1,
+  });
+
+  //? Jenerik Sutun siralama fonksiyonu
+  const handleSort = (arg, type) => {
+    setToggle({ ...toggle, [arg]: toggle[arg] * -1 });
+    setSortedProducts(
+      sortedProducts
+        ?.map((item) => item)
+        .sort((a, b) => {
+          if (type === "date") {
+            return toggle[arg] * (new Date(a[arg]) - new Date(b[arg]));
+          } else if (type === "number") {
+            return toggle[arg] * (a[arg] - b[arg]);
+          } else {
+            if (toggle[arg] === 1) {
+              return b[arg] > a[arg] ? 1 : b[arg] < a[arg] ? -1 : 0;
+            } else {
+              return a[arg] > b[arg] ? 1 : a[arg] < b[arg] ? -1 : 0;
+            }
+          }
+        })
+    );
+  };
+
 };
 
 export default Products;
