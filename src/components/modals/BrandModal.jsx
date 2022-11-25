3ecb2b64117cc970b717b0f99253c14015d68815
AhmetBoyraz01~ <ahmetboyraz.de@gmail.com>
2022-11-25 15:01:14 +0100

    BrandModal.jsx

--- src/components/modals/BrandModal.jsx
+++ src/components/modals/BrandModal.jsx
@@ -24,4 +24,42 @@ export default function BrandModal({ open, setOpen, info, setInfo }) {
     }
     setInfo({});
   };
+
+  return (
+    <Modal
+      open={open}
+      onClose={() => setOpen(false)}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={modalStyle}>
+        <Box sx={flexColumn} component={"form"} onSubmit={handleSubmit}>
+          <TextField
+            label="Brand Name"
+            name="name"
+            id="name"
+            type="text"
+            variant="outlined"
+            value={info?.name || ""}
+            onChange={handleChange}
+            required
+          />
+
+          <TextField
+            label="Image Url"
+            name="image"
+            id="image"
+            type="url"
+            variant="outlined"
+            value={info?.image || ""}
+            onChange={handleChange}
+          />
+
+          <Button type="submit" variant="contained" size="large">
+            Save Brand
+          </Button>
+        </Box>
+      </Box>
+    </Modal>
+  );
 }
