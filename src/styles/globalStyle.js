a45600502990e89ad6cccc04dffcf70ca2c5a824
AhmetBoyraz01~ <ahmetboyraz.de@gmail.com>
2022-11-25 14:31:12 +0100

    modals and globalstyle

--- src/styles/globalStyle.js
+++ src/styles/globalStyle.js
@@ -1,5 +1,34 @@
 export const btnHoverStyle = {
-    cursor: "pointer",
-    "&:hover": { color: "red" },
-  };
-  
+  cursor: "pointer",
+  "&:hover": { color: "red" },
+};
+export const modalStyle = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+export const flexCenter = {
+  display: "flex",
+  justifyContent: "center",
+  gap: 2,
+};
+
+export const flexColumn = {
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  gap: 2,
+};
+
+export const arrowStyle = {
+  display: "flex",
+  justifyContent: "center",
+  cursor: "pointer",
+  "&:hover": { color: "red" },
+};
