6bd3f710e05945e329629248bb46fa1997287d44
AhmetBoyraz01~ <ahmetboyraz.de@gmail.com>
2022-11-27 19:39:48 +0100

    tremor installed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import App from './App';
 import ReactDOM from 'react-dom/client';
+import "@tremor/react/dist/esm/tremor.css";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
