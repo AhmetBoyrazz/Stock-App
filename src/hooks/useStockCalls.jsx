608ee03be5f1b030c05e022ebcc85cd24ba10411
AhmetBoyraz01~ <ahmetboyraz.de@gmail.com>
2022-11-27 19:43:06 +0100

    stockcalls

--- src/hooks/useStockCalls.jsx
+++ src/hooks/useStockCalls.jsx
@@ -1,6 +1,11 @@
 // import { axiosWithToken } from "../service/axiosInstance";
 import { useDispatch } from "react-redux";
-import { fetchFail, fetchStart, getSuccess } from "../features/stockSlice";
+import {
+  fetchFail,
+  fetchStart,
+  getSuccess,
+  getProCatBrandsSuccess,
+} from "../features/stockSlice";
 import useAxios from "./useAxios";
 import { toastSuccessNotify, toastErrorNotify } from "../helper/ToastNotify";
 
@@ -26,6 +31,24 @@ const useStockCalls = () => {
   const getBrands = () => getStockData("brands");
   const getProducts = () => getStockData("products");
 
+  const getProCatBrands = async () => {
+    dispatch(fetchStart());
+    try {
+      const [products, categories, brands] = await Promise.all([
+        axiosWithToken.get("stock/products/"),
+        axiosWithToken.get("stock/categories/"),
+        axiosWithToken.get("stock/brands/"),
+      ]);
+
+      dispatch(
+        getProCatBrandsSuccess([products?.data, categories?.data, brands?.data])
+      );
+    } catch (error) {
+      console.log(error);
+      dispatch(fetchFail());
+    }
+  };
+
   //!------------- DELETE CALLS ----------------
   const deleteStockData = async (url, id) => {
     try {
@@ -40,6 +63,7 @@ const useStockCalls = () => {
 
   const deleteFirm = (id) => deleteStockData("firms", id);
   const deleteBrand = (id) => deleteStockData("brands", id);
+  const deleteProduct = (id) => deleteStockData("products", id);
   //!------------- POST CALLS ----------------
   const postStockData = async (info, url) => {
     try {
@@ -76,9 +100,11 @@ const useStockCalls = () => {
     getSales,
     getCategories,
     getProducts,
+    getProCatBrands,
     getBrands,
     deleteFirm,
     deleteBrand,
+    deleteProduct,
     postFirm,
     postStockData,
     postBrand,
