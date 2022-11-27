0fec9762e876f22b291f180e6aa8b1c7ceb8ade4
AhmetBoyraz01~ <ahmetboyraz.de@gmail.com>
2022-11-27 19:44:04 +0100

    features

--- src/features/stockSlice.jsx
+++ src/features/stockSlice.jsx
@@ -22,6 +22,12 @@ const stockSlice = createSlice({
       state.loading = false;
       state[url] = data;
     },
+    getProCatBrandsSuccess: (state, { payload }) => {
+      state.loading = false;
+      state.products = payload[0];
+      state.categories = payload[1];
+      state.brands = payload[2];
+    },
     fetchFail: (state) => {
       state.loading = false;
       state.error = true;
@@ -29,5 +35,6 @@ const stockSlice = createSlice({
   },
 });
 
-export const { fetchStart, getSuccess, fetchFail } = stockSlice.actions;
+export const { fetchStart, getSuccess, fetchFail, getProCatBrandsSuccess } =
+  stockSlice.actions;
 export default stockSlice.reducer;
