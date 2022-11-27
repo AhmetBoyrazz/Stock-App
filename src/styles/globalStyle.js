1d0e0caef9201c2b9c48d1f0c29035be3eb5b3e1
AhmetBoyraz01~ <ahmetboyraz.de@gmail.com>
2022-11-27 20:00:10 +0100

    src commit

--- src/styles/globalStyle.js
+++ src/styles/globalStyle.js
@@ -21,10 +21,16 @@ export const flexCenter = {
   gap: 2,
 };
 
+export const flex = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  gap: 2,
+};
+
 export const flexColumn = {
   display: "flex",
   flexDirection: "column",
-  justifyContent: "center",
   gap: 2,
 };
 
